0922b9d3157e0936f9bff48c14754de8120bf9f4
ibrhimmese <ibrhim.mese@gmail.com>
2024-10-04 17:22:49 +0300

    Generic Data Hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 
 
 const GameGrid = () => {
-const {games,error,isLoading} = useGames();
+const {data,error,isLoading} = useGames();
 const skeletons = [1,2,3,4,5,6];
 
 return (
@@ -13,7 +13,7 @@ return (
     {error && <Text>{error}</Text>}
     <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} padding='10px' spacing={10}>
         {isLoading && skeletons.map(skeleton=><GameCardSkeleton key={skeleton}></GameCardSkeleton>)}
-        {games.map((game)=>(
+        {data.map((game)=>(
             <GameCard game={game} key={game.id}></GameCard>
         ))}
     </SimpleGrid>
